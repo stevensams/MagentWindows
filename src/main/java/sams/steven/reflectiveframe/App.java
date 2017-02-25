20794d9d3943637a422a72eac43b2e31f2f76c42
Steven Sams <steven.sams@bbc.co.uk>
2017-02-25 21:43:27 +0000

    Basic Maven structure with JMF dependency added

---
+++ src/main/java/sams/steven/reflectiveframe/App.java
@@ -0,0 +1,13 @@
+package sams.steven.reflectiveframe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
